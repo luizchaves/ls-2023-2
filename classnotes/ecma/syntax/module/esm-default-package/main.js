0fbbd8f336b328a1d01410691579b1cedda62c36
Luiz <lucachaves@gmail.com>
2023-08-21 14:48:37 -0300

    esm default, npm

---
+++ classnotes/ecma/syntax/module/esm-default-package/main.js
@@ -0,0 +1,4 @@
+import lib, { sum } from './lib.js';
+
+console.log(sum(1, 1));
+console.log(lib.minus(1, 1));
---
+++ classnotes/ecma/syntax/module/esm-npm-package/main.js
@@ -0,0 +1,6 @@
+import lib, { sum } from './lib.js';
+import { sqrt } from 'mathjs';
+
+console.log(sum(1, 1));
+console.log(lib.minus(1, 1));
+console.log(sqrt(4));
