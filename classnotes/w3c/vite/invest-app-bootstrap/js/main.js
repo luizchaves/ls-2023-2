92e81a1f5a6efece51b1bbf8d48fceb440fdd3a7
Luiz <lucachaves@gmail.com>
2023-09-27 14:49:44 -0300

    package e vite

---
+++ classnotes/w3c/vite/invest-app-bootstrap/js/main.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import { investments } from './data';
+import { InvestmentCard } from './components/InvestmentCard';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const investmentsGrid = document.querySelector('.investments');
+
+investmentsGrid.innerHTML = investments.map(InvestmentCard).join('');
